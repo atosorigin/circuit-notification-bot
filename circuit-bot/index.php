e1cfceb8360a150259a00eadaccce7f5fc02a07f
Christoph Schulz <christoph.2.schulz@atos.net>
2017-09-14 16:36:41 +0200

    [bot] cleanup

--- circuit-bot/index.php
+++ circuit-bot/index.php
@@ -6,15 +6,20 @@ use ICanBoogie\Storage\FileStorage;
 
 if(!function_exists('circuit_bot'))
 {
-    define('BASE_URL', 'https://eu.yourcircuit.com/');
+    define('TOKEN_ENDPOINT', 'https://eu.yourcircuit.com/oauth/token');
     define('TOKEN_KEY', 'token_response');
+
+    define('FILTER_WAKEUP', 'wakeup');
+    define('FILTER_WAKEUP_ADV', 'wakeup_advanced');
+
+    define('ACTION_PARENT_ID', 'parent_id');
 
     function circuit_bot($config)
     {
         global $hooks;
 
         $storage = new FileStorage(__DIR__);
-        $tokenEndpoint = BASE_URL . 'oauth/token';
+        $conv_id = $config['conId'];
 
         function print_conv_item($conv_item)
         {
@@ -23,6 +28,7 @@ if(!function_exists('circuit_bot'))
                 'Content ', $conv_item['text']['content'], PHP_EOL, PHP_EOL;
         }
 
+        // Try to reuse OAuth token, request new one if expired.
         if($response = $storage->retrieve(TOKEN_KEY))
         {
             echo 'Token loaded', PHP_EOL;
@@ -33,7 +39,7 @@ if(!function_exists('circuit_bot'))
             echo 'No token found, requesting new one...', PHP_EOL;
 
             $response = (new OAuth2\Client($config['client']['id'], $config['client']['secret']))
-                ->getAccessToken($tokenEndpoint, 'client_credentials', ['scope' => 'ALL'])
+                ->getAccessToken(TOKEN_ENDPOINT, 'client_credentials', ['scope' => 'ALL'])
                 ['result'];
 
             $storage->store(TOKEN_KEY, $response, $response['expires_in'] - 10 /* just to be sure */);
@@ -41,29 +47,19 @@ if(!function_exists('circuit_bot'))
             $token = $response['access_token'];
         }
 
-        // Configure Host and OAuth2 access token for authorization
-        Swagger\Client\Configuration::getDefaultConfiguration()
-            ->setAccessToken($token)
-            ->setHost(BASE_URL . 'rest/v2');
-
-        $hooks->add_filter('wakeup', 'wakeup_ex');
-
-        function wakeup_ex($ary){
-            return array_merge($ary, ['integrated filter']);
-        }
+        // Configure OAuth2 access token for authorization
+        Swagger\Client\Configuration::getDefaultConfiguration()->setAccessToken($token);
 
         $api_instance = new Swagger\Client\Api\MessagingBasicApi();
-        $conv_id = $config['conId'];
 
-        $wakeup = $hooks->apply_filters('wakeup', []);
-        $wakeup_advanced = $hooks->apply_filters('wakeup_advanced', []);
+        $wakeup = $hooks->apply_filters(FILTER_WAKEUP, []);
+        $wakeup_advanced = $hooks->apply_filters(FILTER_WAKEUP_ADV, []);
 
         foreach($wakeup as $key => $content)
         {
             try
             {
-                $result = $api_instance->addTextItem($conv_id, $content);
-                print_conv_item($result);
+                print_conv_item($result = $api_instance->addTextItem($conv_id, $content));
             }
             catch (Exception $e)
             {
@@ -86,14 +82,14 @@ if(!function_exists('circuit_bot'))
 
                     $result = $api_instance->addTextItem($conv_id, $msg_adv->message);
 
-                    $hooks->do_action('parent_id', $msg_adv->id, $result['item_id']);
+                    $hooks->do_action(ACTION_PARENT_ID, $msg_adv->id, $result['item_id']);
 
                 }
                 print_conv_item($result);
             }
             catch (Exception $e)
             {
-                echo 'Exception when calling MessagingBasicApi->addTextItem: ', $e->getMessage(), PHP_EOL;
+                echo 'Exception when calling MessagingBasicApi->addTextItem/addTextItemWithParent: ', $e->getMessage(), PHP_EOL;
             }
 
         }
