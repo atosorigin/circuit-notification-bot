bb720a5aa228bc17fdd49aedc4b29ac74e2510ea
Christoph Schulz <christoph.2.schulz@atos.net>
2017-09-19 15:07:45 +0200

    polling multiple feeds [whitespace change]

--- circuit-plugin-feed-poll/index.php
+++ circuit-plugin-feed-poll/index.php
@@ -14,55 +14,59 @@ if(!function_exists('wakeup_feed'))
 
         $my_config = $config['plugins']['feed_poll'];
 
-        $feed_url = $my_config['feed_url'];
-        $feed_auth = $my_config['feed_auth'];
-        $auth_url = $my_config['auth_url'];
-
-        $storage = new ICanBoogie\Storage\FileStorage(__DIR__);
-        $feed_mri_token = 'mri_' . sha1($feed_url); // mri most recent id; hash to sanitize
-
-        $client_cfg = [];
-
-        if(in_array('cookies', $feed_auth))
+        foreach($my_config['feeds'] as $my_feed)
         {
-            $cookies = $my_config['cookies'];
-            $client_cfg['defaults'] = [
-                'cookies' => GuzzleHttp\Cookie\CookieJar::fromArray($cookies, parse_url($auth_url, PHP_URL_HOST))
-            ];
-        }
 
-        $client = new GuzzleHttp\Client($client_cfg);
+            $feed_url = $my_feed['feed_url'];
+            $feed_auth = $my_feed['feed_auth'];
+            $auth_url = $my_feed['auth_url'];
 
-        if(in_array('form', $feed_auth))
-        {
-            $client->post($auth_url, [
-                'body' => $my_config['form_fields'],
-                'allow_redirects' => true
-            ]);
-        }
+            $storage = new ICanBoogie\Storage\FileStorage(__DIR__);
+            $feed_mri_token = 'mri_' . sha1($feed_url); // mri most recent id; hash to sanitize
 
-        $response = $client->get($feed_url);
+            $client_cfg = [];
 
-        $feed = new SimplePie();
-
-        $feed->set_raw_data((string) ($response->getBody()));
-        $feed->init();
-
-        $mri = $storage->retrieve($feed_mri_token);
-        $id0 = $feed->get_item(0)->get_id();
-
-        if($id0 != $mri)
-        {
-            foreach ($feed->get_items() as $item)
+            if(in_array('cookies', $feed_auth))
             {
-                if($item->get_id() == $mri) break;
-                $ary[] = $item->get_title() . ': ' . preg_replace('/\\s+/', ' ', $item->get_description()); // circuit does not like line breaks
+                $cookies = $my_feed['cookies'];
+                $client_cfg['defaults'] = [
+                    'cookies' => GuzzleHttp\Cookie\CookieJar::fromArray($cookies, parse_url($auth_url, PHP_URL_HOST))
+                ];
             }
-            $mri = $id0;
-        }
-        // else nothing to do
 
-        $storage->store($feed_mri_token, $mri);
+            $client = new GuzzleHttp\Client($client_cfg);
+
+            if(in_array('form', $feed_auth))
+            {
+                $client->post($auth_url, [
+                    'body' => $my_feed['form_fields'],
+                    'allow_redirects' => true
+                ]);
+            }
+
+            $response = $client->get($feed_url);
+
+            $feed = new SimplePie();
+
+            $feed->set_raw_data((string) ($response->getBody()));
+            $feed->init();
+
+            $mri = $storage->retrieve($feed_mri_token);
+            $id0 = $feed->get_item(0)->get_id();
+
+            if($id0 != $mri)
+            {
+                foreach ($feed->get_items() as $item)
+                {
+                    if($item->get_id() == $mri) break;
+                    $ary[] = $item->get_title() . ': ' . preg_replace('/\\s+/', ' ', $item->get_description()); // circuit does not like line breaks
+                }
+                $mri = $id0;
+            }
+            // else nothing to do
+
+            $storage->store($feed_mri_token, $mri);
+        }
         return $ary;
     }
 
